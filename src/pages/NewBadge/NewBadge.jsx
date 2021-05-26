f815d07c203f1098145d7ce18565b1eaeb3156de
Saul Uriel <sauluriel1402@gmail.com>
2021-05-25 18:48:41 -0600

    Adding the hero to the NewBadge

---
+++ src/pages/NewBadge/NewBadge.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NewBadge.css'
+import Hero from '../../components/Hero'
+
+class NewBadge extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <Hero h={'15vh'}></Hero>
+                <h1>New Badge</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default NewBadge
