077e343ca3d20167497cb306a1e77d0068d410ec
Saul Uriel <sauluriel1402@gmail.com>
2021-05-25 19:32:10 -0600

    Adding the new dynamic the new badge to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
  import React from 'react'
  import Layout from './pages/layout/Layout'
  import Main from './pages/main/Main'
+ import NewBadge from './pages/NewBadge/NewBadge'
 
 function App() {
   return (
       <React.Fragment>
         <Layout>
-          <Main tittle={'Welcome Fella'}></Main>
+          <NewBadge>
+            
+          </NewBadge>
         </Layout>
-
       </React.Fragment>
   );
 }
