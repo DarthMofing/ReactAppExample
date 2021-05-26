9596d4100862be5c821cb903444b5d07aa8229cd
Saul Uriel <sauluriel1402@gmail.com>
2021-05-25 20:02:13 -0600

    All the form structure created

--- src/components/BadgeForm.jsx
+++ src/components/BadgeForm.jsx
@@ -1,11 +1,89 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './style/BadgeForm.css'
 
-class BadgeForm extends React.Component{
-    render(){
-        return(
+class BadgeForm extends React.Component {
+    render() {
+        return (
             <React.Fragment>
-                <h1>BadgeForm</h1>
+                <div className="BadgeForm m-5">
+                    <form onSubmit={this.props.onSubmit}>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Name
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="name"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.name}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Profile Picture URL
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="name"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.name}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Name
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="profile_pic_url"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.name}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Badge Hero URL
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="badge_hero_url"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.name}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                Age
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="age"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.name}
+                            />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label className="mb-1">
+                                City
+                            </label>
+                            <input 
+                                type="text"
+                                className="form-control"
+                                name="city"
+                                onChange={this.props.onChange}
+                                value={this.props.formValues.name}
+                            />
+                        </div>
+                        <button type="submit" className="Submit__button">
+                            Submit
+                        </button>
+                    </form>
+                </div>
             </React.Fragment>
         );
     }
