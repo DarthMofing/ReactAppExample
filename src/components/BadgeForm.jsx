10d5e1fb39b1e72350b012e9d0c953b85482ad2b
Saul Uriel <sauluriel1402@gmail.com>
2021-05-25 19:31:19 -0600

    Creating a form for the badge

---
+++ src/components/BadgeForm.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import './style/BadgeForm.css'
+
+class BadgeForm extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>BadgeForm</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default BadgeForm
