a1ec6f49ff329fc25231a4014f6991005fefedb6
Saul Uriel <sauluriel1402@gmail.com>
2021-06-07 09:10:03 -0600

    loading animation skeleton and badges file

--- src/pages/Badges/Badges.jsx
+++ src/pages/Badges/Badges.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import api from '../../libs/api'
+import SkeletonItem from '../../components/SkeletonItem'
 import './Badges.css'
 
 class Badges extends React.Component {
@@ -9,10 +11,37 @@ class Badges extends React.Component {
         data:undefined
     }
 
+    componentDidMount() {
+        this.fetchData();
+        this.setFetchInterval();
+    }
+
+    fetchData = async() => {
+        this.setState({loading:true, error:null})
+        try {
+            const data = await api.badges.list()
+            data.reverse()
+            this.setState({loading:false, data:data})
+        }catch(error) {
+            this.setState({loading:false, error:error, data:[]})
+        }
+    }
+
+    setFetchInterval() {
+        this.interval = setInterval(this.fetchData, 3000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
     render() {
+        if(this.state.loading===true && !this.state.data) {
+            return <SkeletonItem></SkeletonItem>
+        }
         return (
             <React.Fragment>
-
+                <h1>All badges</h1>
             </React.Fragment>
         );
     }
