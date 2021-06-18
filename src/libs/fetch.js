fd1dd07771248cb57bb34a54442e406cd5fefde9
Saul Uriel <sauluriel1402@gmail.com>
2021-06-18 12:34:52 -0600

    yarn deleted

--- src/libs/fetch.js
+++ src/libs/fetch.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:5000';
+const DEV_URL = 'http://localhost:5000';
+const BASE_URL = "https://badges-api-sb.herokuapp.com"
 
 async function callApi(endpoint, options = {}) {
   options.headers = {
